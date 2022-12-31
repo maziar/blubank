cf0b3101e5276cad0ed78a49a085c309321ad2bd
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2022-12-31 13:17:19 +0330

    + Add binding for country list view controller

---
+++ Blu/Module/CountryList/View/CountryListViewController+Binding.swift
@@ -0,0 +1,33 @@
+//
+//  CountryListViewController+Binding.swift
+//  Blu
+//
+//  Created by Maziar Saadatfar on 12/29/22.
+//
+
+import Foundation
+
+extension CountryListViewController {
+    func setUpBinding() {
+        bindCountryListViewModel()
+    }
+    
+    func bindCountryListViewModel() {
+        viewModel.countryListChangeHandler = { [weak self] change in
+            guard let strongSelf = self else { return }
+            switch change {
+            case .didChangeNetworkActivityStatus(_):
+                break
+            case .didSuccess:
+                strongSelf.tableView.cr.endHeaderRefresh()
+                strongSelf.tableView.cr.endLoadingMore()
+                strongSelf.viewModel.fetchAllCountryList()
+                strongSelf.tableView.reloadData()
+            case .didError(let error):
+                strongSelf.tableView.cr.endHeaderRefresh()
+                strongSelf.tableView.cr.endLoadingMore()
+                print(error)
+            }
+        }
+    }
+}
