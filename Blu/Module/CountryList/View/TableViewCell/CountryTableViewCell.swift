a9c46910b0bd2d014b9838435925ee10cf7c91ca
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2022-12-31 13:18:17 +0330

    + Add country list table view cell

---
+++ Blu/Module/CountryList/View/TableViewCell/CountryTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  CountryTableViewCell.swift
+//  Blu
+//
+//  Created by Maziar Saadatfar on 12/27/22.
+//
+
+import UIKit
+import MarqueeLabel
+import SDWebImage
+import UIView_Shimmer
+
+class CountryTableViewCell: BaseCountryCell, ShimmeringViewProtocol {
+    @IBOutlet weak var flagImageView: UIImageView!
+    @IBOutlet weak var countryShimmerLabel: UILabel!
+    @IBOutlet weak var countryLabel: MarqueeLabel!
+    
+    var shimmeringAnimatedItems: [UIView] {
+        [
+            countryShimmerLabel,
+            flagImageView
+        ]
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.showShimmer(show: true)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func config(country: Country) {
+        let presenter = CountryPresenter(country: country)
+        countryLabel.text = presenter.name
+        flagImageView.sd_setImage(with: URL(string: presenter.flag),
+                                  placeholderImage: UIImage(named: Image.placeholder.rawValue))
+        let accessoryType: AccessoryType = country.selected ? .checkmark : .none
+        self.accessoryType = accessoryType
+        self.showShimmer(show: false)
+    }
+}
+
+extension CountryTableViewCell {
+    class func register(for tableView: UITableView) {
+        tableView.register(UINib(nibName: CountryTableViewCell.nameOfClass, bundle: Bundle.main),
+                           forCellReuseIdentifier: CountryTableViewCell.nameOfClass)
+    }
+}
