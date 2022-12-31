552f136b9a9c9cde8e26ca07a0bbf0c56766aff1
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2022-12-31 13:16:08 +0330

    + Add country list view controller

---
+++ Blu/Module/CountryList/View/CountryListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CountryListViewController.swift
+//  Blu
+//
+//  Created by Maziar Saadatfar on 12/27/22.
+//
+
+import UIKit
+
+class CountryListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var searchController: UISearchController!
+    var searchViewController: SearchViewController!
+    
+    var viewModel = CountryListViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+        addDoneButton()
+        setUpBinding()
+        setup()
+        setupSearchViewController()
+        viewModel.fetch()
+    }
+    
+    func setup() {
+        self.title = Label.countryList.rawValue
+    }
+    
+    func addDoneButton() {
+        let doneButton = UIBarButtonItem(title: Label.done.rawValue,
+                                        style: .done,
+                                        target: self,
+                                        action: #selector(dismissView))
+        self.navigationItem.leftBarButtonItems = [doneButton]
+    }
+    
+    @objc
+    func dismissView() {
+        self.dismiss(animated: true)
+    }
+}
