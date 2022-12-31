e8dd2bd20ddd1238cad80a693c28084938a3aeeb
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2022-12-31 13:14:21 +0330

    + Add country list api response for mapping data from api

---
+++ Blu/Module/CountryList/ServiceDetails/Response/CountryListResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CountryListResponse.swift
+//  Blu
+//
+//  Created by Maziar Saadatfar on 12/28/22.
+//
+
+import Foundation
+import ObjectMapper
+
+public struct CountryListResponse {
+    public var countries: [CountryModel]!
+}
